aa6f6264cf859c1f8fe7addbbede095a35776b2b
jessinmacdon <jessinmacdon@gmail.com>
2022-02-21 22:56:47 +0100

    creating a models.js files and adding mongoose schema for movies and users collection for myFlixDB

---
+++ models.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+
+// defining the schema for movies
+let movieSchema = mongoose.Schema ({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String,
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    ImagePath: String,
+    Featured: Boolean
+});
+
+//defining the Scema for users
+let userSchema = mongoose.Schema ({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+//populating the users favouriteMovies array
+function addFavouriteMovies (Username) {
+    return User.findOne({ Username: Username })
+    .populate('movies').exec((err, Users) => {
+        console.log('Populated User' + Movie.Title);
+    })
+}
+
+//creating models
+let Movie = mongoose.model('Movie', movieSchema);
+
+let User = mongoose.model('User', userSchema);
+
+//esporting modules
+module.exports.Movie = Movie;
+module.exports.User = User;
